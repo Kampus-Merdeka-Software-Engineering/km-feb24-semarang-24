30436d5df6f334e0df2acde3b11bc16fbc1e9f58
Sabna Devi Kumalasari <103269680+sabnadk@users.noreply.github.com>
2024-05-31 22:23:51 +0700

    nambahin halaman login

---
+++ js/login.js
@@ -0,0 +1,59 @@
+const loginBtn = document.querySelector("#login");
+const registerBtn = document.querySelector("#register");
+const loginForm = document.querySelector(".login-form");
+const registerForm = document.querySelector(".register-form");
+
+
+loginBtn.addEventListener('click', () => {
+    loginBtn.style.backgroundColor = "#21264D";
+    registerBtn.style.backgroundColor = "rgba(255, 255, 255, 0.2)";
+
+    loginForm.style.left = "50%";
+    registerForm.style.left = "-50%";
+
+    loginForm.style.opacity = 1;
+    registerForm.style.opacity = 0;
+
+    document.querySelector(".col-1").style.borderRadius = "0 30% 20% 0";
+})
+
+
+registerBtn.addEventListener('click', () => {
+    loginBtn.style.backgroundColor = "rgba(255, 255, 255, 0.2)";
+    registerBtn.style.backgroundColor = "#21264D";
+
+    loginForm.style.left = "150%";
+    registerForm.style.left = "50%";
+
+    loginForm.style.opacity = 0;
+    registerForm.style.opacity = 1;
+
+    document.querySelector(".col-1").style.borderRadius = "0 20% 30% 0";
+})
+
+// Validation and Alert
+const loginSubmitBtn = loginForm.querySelector(".input-submit a");
+const registerSubmitBtn = registerForm.querySelector(".input-submit a");
+
+loginSubmitBtn.addEventListener('click', (event) => {
+    const username = loginForm.querySelector('input[type="text"]').value;
+    const password = loginForm.querySelector('input[type="password"]').value;
+
+    if (username === "" || password === "") {
+        event.preventDefault();
+        alert("Please fill in all fields in the login Form.");
+    }
+})
+
+registerSubmitBtn.addEventListener('click', (event) => {
+    const email = registerForm.querySelector('input[type="text"]').value;
+    const username = registerForm.querySelector('input[type="text"]').value;
+    const password = registerForm.querySelector('input[type="password"]').value;
+
+    if (email === "" || username === "" || password === "") {
+        event.preventDefault();
+        alert("Please fill in all fields in the registration Form.");
+    }
+})
+
+
